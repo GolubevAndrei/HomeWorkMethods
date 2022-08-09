3ccc61a7df1764ac939077b7e67c558df538ac1b
GolubevAndrey <golubev_andrei87@mail.ru>
2022-08-09 13:49:16 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    public static void leapYear(int year) {
+        year = 2022;
+        if (((year % 4 == 0) && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
+    }
     public static void main(String[] args) {
 //      1 Задание
+        int year = 2022;
+        leapYear(year);
     }
 }
