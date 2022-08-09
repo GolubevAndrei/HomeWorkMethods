6f17abacf542fc5baa4af374f572db3a43313acc
GolubevAndrey <golubev_andrei87@mail.ru>
2022-08-09 13:57:52 +0300

    Второе задание

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void leapYear(int year) {
-        year = 2022;
         if (((year % 4 == 0) && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год является високосным.");
         } else {
             System.out.println(year + " год не является високосным.");
         }
     }
+
+    public static void OS(int clientOS, int yearOS) {
+        if (clientOS == 1 && yearOS > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && yearOS > 2015){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
     public static void main(String[] args) {
+
 //      1 Задание
-        int year = 2022;
+
+        int year = LocalDate.now().getYear();
         leapYear(year);
+
+//      2 Задание
+
+        int yearOS = LocalDate.now().getYear();
+        int clientOS = 1;
+        OS(clientOS, yearOS);
     }
 }
